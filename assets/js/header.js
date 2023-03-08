172f1ea9764c3b8db3d4cea2ed489efe802e24f3
AlanCampos <alancamposdeveloper@gmail.com>
2023-03-08 14:16:38 -0400

    add menu mobile funcional

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,1 +1,9 @@
-//Script navigation bar
+//Menu Mobile - add classe menu-active
+
+const btnMobile = document.getElementById('btn-menuMobile');
+const menu = document.querySelector('.header__nav');
+
+btnMobile.addEventListener('click',() => {
+  menu.classList.toggle('menu-active')
+});
+
